c1804716135eb4d33aafe895b7356a88fc47c744
Joseatorres513 <Joseatorres513>
2024-12-07 18:09:34 -0500

    FSDI 106 Assignmnet3

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,52 @@
-function saveTask(){
+function saveTask()
+{
     console.log("saving task");
+    //get values
+
+    const title = $("#txtTitle").val();
+    const desc = $("#txtDescription").val();
+    const color = $("#selColor").val();
+    const date = $("#selDate").val();
+    const status = $("#selStatus").val();
+    const budget = $("#numBudget").val();
+    console.log(title,desc,color,date,status,budget);
+
+    //build an object
+    let taskToSave = new Task(title,desc,color,date,status,budget);
+    console.log(taskToSave); 
+    displayTask(taskToSave);
+}
+    //save to server
+
+    //display the task
+function displayTask(task)
+{
+    console.log(task)
+    let syntax = `<div class="task">
+        <h5>${task.title}</h5>
+        <p>${task.desc}</p>
+        </div>
+        <div><label>${task.status}</label></div>
+        <div><label>${task.date}</label></div>
+        <div><label>${task.budget}</label></div>`
+        ;
+
+
+
+        $(".list-class").append(syntax);
+}
+
+function testRequest() {
+    $.ajax({
+        type: "get",
+        url: "http://fsdiapi.azurewebsites.net",
+        success: function(response){
+            console.log(response);
+        },
+        error: function(error){
+            console.log(error)
+        }
+    });
 }
 
 function init() {
@@ -7,7 +54,7 @@ function init() {
     //load data
 
     //hook the events
-    $("#btnSave").onClick(saveTask);
+    $("#btnSave").click(saveTask);
 }
 
 window.onload = init;
